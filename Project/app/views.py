f9a20a24d2a26111c5c45b197addcee4d36923c3
vldmrvlnzl <vldmrvlnzl@gmail.com>
2024-10-06 08:17:44 +0800

    commit

---
+++ Project/app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+  template_name = 'app/home.html'
+
+class AboutPageView(TemplateView):
+  template_name = 'app/about.html'
+
+class ContactPageView(TemplateView):
+  template_name = 'app/contact.html'
